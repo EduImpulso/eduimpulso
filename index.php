ff2ae1de968826aa21bf7ebc499fcf701b21ee07
Gustavo Castanho <65131471+duartecgustavo@users.noreply.github.com>
2021-01-09 19:02:17 -0300

    fix:update-base
    Atualizações:
    -Index (padrão) (HTML)
    -Menu (padrão)
    -CSS GLOBAL

--- index.php
+++ index.php
@@ -1,47 +1,44 @@
 <?php
-    //require_once('./servidor/Conection.php');
-    require_once('./nav/menu.html');
+//require_once('./servidor/Conection.php');
+require_once('./nav/menu.html');
 ?>
-        <div class="bannerHome container-fluid" id="desktop">
 
-            <div class="container d-flex align-items-center p-5">
+<div class="bannerHome container-fluid" id="desktop" id="topo">
 
-                <h1 class="p-4">
-                MIRE O OLHAR NO<br/>
-                SEU FUTURO!<br/>
-                <a class="form-text linkHome" href="/sobre">Nos conheça melhor <img width="12" src="./imgs/index/seta-direita.svg"/></a>
-                </h1>
-                
-            </div>
+<div class="container d-flex align-items-center p-5">
 
-        </div>
-        <div class="d-flex justify-content-center align-items-center my-5">
-            <div class="borderRadius m-auto p-3 col-8 col-sm-10 col-xl-4 bg-purple">
-                <h1 class="camposLogin text-center pb-4">LOGIN</h1>
-                <form>
-                <div className="form-group">
-                    <label className="my-2 form-label" for="e-mail" style="color: white">Digite seu e-mail</label>
-                    <input className="imputForm form-control has-validation" type="e-mail"id="email" aria-describedby="emailHelp"
-                            placeholder="email@email.com" name="email"/>
+    <h1 class="p-4">
+        MIRE O OLHAR NO<br /> SEU FUTURO!<br />
+    </h1>
+
+</div>
+
+</div>
+<div class="d-flex justify-content-center align-items-center my-5">
+        <div class="borderRadius bg-purple m-auto p-3 col-8 col-sm-10 col-xl-4">
+            <h1 class="camposLogin text-white text-center pb-4">LOGIN</h1>
+            <form>
+                <div class="form-group">
+                    <label class="my-2 form-label" for="e-mail" style="color: white">Digite seu e-mail</label>
+                    <input class="imputForm form-control has-validation" type="e-mail" id="email" aria-describedby="emailHelp" placeholder="email@email.com" name="email" />
                 </div>
-                <div className="form-group">
-                    <label className="my-2 form-label" for="password" style="color: white">Digite seu e-mail</label>
-                    <input className="imputForm form-control has-validation" type="password"id="password"
-                            placeholder="email@email.com" name="password"/>
-                </div>      
-                
-                <a class="alternativeLink form-text " href="/cadastro">Cadastre-se aqui!</a>
+                <div class="form-group">
+                    <label class="my-2 form-label" for="password" style="color: white">Digite seu e-mail</label>
+                    <input class="imputForm form-control has-validation" type="password" id="password" placeholder="email@email.com" name="password" />
+                </div>
 
-                <button className="buttonForm btn mt-3">
-                    Enviar
-                </button>
-                </form>
-            </div>
-            <div class="col-12 col-sm-12 col-xl-6 d-flex justify-content-center my-4" id="">
-                <img src="./imgs/index/capa-home-login.jpg" width="80%" alt="login"/>
-            </div>
+                <a class="linkCadastro form-text " href="/cadastro">Cadastre-se aqui!</a>
+
+                <button class="buttonForm btn mt-3">
+                Enviar
+            </button>
+            </form>
         </div>
+        <div class="col-12 col-sm-12 col-xl-6 d-flex justify-content-center my-4" id="">
+            <img src="./imgs/index/capa-home-login.jpg" width="80%" alt="login" />
+        </div>
+    </div>
 <?php
-    //require_once('./servidor/Conection.php');
-    require_once('./nav/footer.html');
+//require_once('./servidor/Conection.php');
+require_once('./nav/footer.html');
 ?>
