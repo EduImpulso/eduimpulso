f0265616f9a6a153df03622c8a850b335f2cc752
Luzia Almeida <lu.1998@hotmail.com>
2021-01-19 18:59:04 -0300

    fix: cursinhos

---
+++ cursinhos.php
@@ -0,0 +1,263 @@
+<?php require_once('./nav/menu.html') ?>
+
+<body style="background-color: #a25fd1;">
+
+    
+
+    <div class="container-fluid">
+
+        <main class="row" style="background-color: white;"> 
+        <div class ="col-12 text-center my-4">
+        </div>
+
+            <div class="container">
+
+                <div class="jumbotron jumbotron-fluid text-align-justify" style="background-color:#C6A6D9;">
+                    <div class="container">
+                        <h1 class="display-4">Cursinhos preparatórios</h1>
+                        <p class="lead "> No processo de preparação para conseguir aquela sonhada vaga na Universidade, temos certeza que você já cogitou fazer um cursinho preparatório.
+                        De maneira geral, o cursinho é um curso preparatório para o vestibular. Ele pode ser oferecido por instituições de ensino sob as mais variadas formas, como o curso extensivo, com duração de um ano, o semiextensivo, que leva um semestre para ser concluído e as turmas de maio, modelo intermediário que podem trabalhar exclusivamente com pré-vestibulares ou também com ensino médio, e cursinhos gratuitos, geralmente oferecidos por grupos interessados no trabalho voluntário.
+                          É possível fazer cursinhos nas modalidades presencial e online, sendo esta uma solução cada vez mais procurada em virtude da evolução da tecnologia. 
+                       
+                        Aqui vamos mostrar as principais instituições pagas e gratuitas pra você mergulhar de cabeça nos cursinhos preparatórios e ingressar naquela tão sonhada Universidade :) 
+                    </div>
+                 </div>
+            </div>
+    <div class="container">
+        <div class="card-container">
+            <div class="card">
+                <div class="front">
+                    <h2 class ="text-white my-4">FEAUSP</h2>
+                </div>
+                <div class="back">
+                    <h1 class="text-info"> FEAUSP </h1>
+                        O Cursinho FEAUSP é um projeto social sem fins lucrativos. Atuando desde 2000 como um cursinho pré-vestibular popular, tem a missão de promover a democratização da educação promovendo o ingresso de alunos em universidades de qualidade. <br/>
+                            <p><strong>Para mais informações sobre o cursinho FEAUSP</strong></p>
+                                <a style="margin-top: 10px;"href="https://www.cursinhofeausp.com.br/increver-se" class="btn btn-success"> Clique aqui </a>
+                 </div>
+               </div>
+        </div>
+
+        <div class="card-container">
+            <div class="card">
+                <div class="front">
+                <h2 class ="text-white my-4"> ARCADAS VESTIBULARES</h2>
+                </div>
+                <div class="back">
+                    <h1 class="text-info"> ARCADAS VESTIBULARES</h1>
+                    O Arcadas Vestibulares é um cursinho pré-vestibular gratuito, criado por alunos da faculdade de Direito da USP. Situado no centro de São Paulo, tem como objetivo a democratização do ensino superior no país
+                    Ao longo de sua história, o Arcadas buscou por expandir e profissionalizar cada vez mais suas atividades.
+                    <p><strong>Para mais informações sobre o cursinho Arcadas</strong> </p>
+                    <a style="margin-top: 10px;"href="https://www.cursinhofeausp.com.br/increver-se" class="btn btn-success"> Clique aqui </a>
+                 </div>
+            </div>
+        </div>
+
+        <div class="card-container">
+            <div class="card">
+                <div class="front">
+                <h2 class ="text-white my-4"> CURSINHO DA POLI</h2>
+                </div>
+                <div class="back">
+                    <h1 class="text-info"> CURSINHO DA POLI</h1>
+                    O Cursinho da Poli é um projeto de educação idealizado pelo Grêmio Politécnico da USP em 1987.
+                    Sua missão é transformar a vida de pessoas, promovendo a formação integral por meio de ações, projetos e experiências, oportunizando o acesso às melhores universidades, ao mundo do trabalho e ao pleno exercício da cidadania.</p>
+                    <p><strong> Para mais informações sobre o cursinho da Poli</strong></p>
+                        <a style="margin-top: 10px;"href="https://cursinhodapolionline.com.br/" class="btn btn-success"> Clique aqui </a>
+                 </div>
+            </div>
+        </div>
+
+        <div class="card-container">
+            <div class="card">
+                <div class="front">
+                <h2 class ="text-white my-4"> CUJA</h2>
+                </div>
+                <div class="back">
+                    <h1 class="text-info"> CUJA</h1>
+                    O Cursinho Pré-Vestibular Jeaninne Aboulafia (CUJA) é um cursinho popular gratuito que atua como um projeto de ação social do campus São Paulo da Universidade Federal de São Paulo (UNIFESP). Desde nossa fundação, há 18 anos, com o objetivo de  revisar o conteúdo do Ensino Médio com alunos provenientes de escolas públicas e/ou de classes menos favorecidas, buscando a sua preparação para os vestibulares e sua inserção no ambiente acadêmico
+                    <p><strong> Para mais informações sobre o cursinhos do Cuja</strong></p>
+                    <a style="margin-top: 10px;"href="https://www.cujaunifesp.com/processo-seletivo/" class="btn btn-success"> Clique aqui </a>
+                 </div>
+            </div>
+        </div>
+
+        <div class="card-container">
+            <div class="card">
+                <div class="front">
+                <h2 class ="text-white my-4"> CURSINHO ETAPA </h2>
+                </div>
+                <div class="back">
+                    <h1 class="text-info"> CURSINHO ETAPA</h1>
+                    ETAPA é um colégio particular com sede em São Paulo. Em 2015, o colégio obteve o segundo lugar em notas do ENEM, sendo superado apenas pelo Colégio Objetivo.  O Etapa oferece apoio maior, uma estrutura forte e as melhores condições para que você desenvolva mais de você em você mesmo.
+                     <p><strong> Para mais informações sobre o cursinho do Etapa </strong></p>
+                     <a style="margin-top: 10px;"href="https://www.etapa.com.br/home//processo-seletivo/" class="btn btn-success"> Clique aqui </a>
+                 </div>
+            </div>
+        </div>
+
+        <div class="card-container">
+            <div class="card">
+                <div class="front">
+                <h2 class ="text-white my-4"> CURSINHO HENFIL</h2>
+                </div>
+                <div class="back">
+                    <h1 class="text-info"> CURSINHO HENFIL</h1>
+                    Aluno do Cursinho Henfil tem acesso grátis ao Descomplica. 
+                     O Cursinho Henfil tem uma proposta completa, moderna e inovadora, que ajuda em seu preparo para o vestibular!
+                     <p><strong> Para mais informações sobre o cursinho da Henfil </strong><p>
+                     <a style="margin-top: 10px;"href="https://cursohenfil.com.br/" class="btn btn-success"> Clique aqui </a>
+                 </div>
+            </div>
+        </div>
+
+        <div class="card-container">
+            <div class="card">
+                <div class="front">
+                <h2 class ="text-white my-4"> CURSINHO OBJETIVO</h2>
+                </div>
+                <div class="back">
+                    <h1 class="text-info"> CURSINHO OBJETIVO</h1>
+                    Os professores do OBJETIVO, em plantão permanente, resolvem e comentam todos os grandes vestibulares, analisando as tendências e inovações para incorporá-las, de imediato, aos cadernos de atividades e aos livros.
+                    <p><strong> Para mais informações sobre o cursinho do Objetivo</strong></p>
+                    <a style="margin-top: 10px;"href="https://www.curso-objetivo.br/" class="btn btn-success"> Clique aqui </a>
+                 </div>
+            </div>
+        </div>
+
+        <div class="card-container">
+            <div class="card">
+                <div class="front">
+                <h2 class ="text-white my-4">CURSINHO EACH</h2>
+                </div>
+                <div class="back">
+                    <h1 class="text-info"> CURSINHO EACH</h1>
+                    O Cursinho Popular EACH/USP, organização sem fins lucrativos formada por alunos da Escola, o objetivo da entidade é preparar estudantes de baixa renda para o vestibular por meio de aulas, plantões, simulados e palestras gratuitas.
+                    <p><strong> Para mais informações sobre o cursinho EACH</strong></p>
+                    <a style="margin-top: 10px;"href="https://www.cursinhoeach.com.br/" class="btn btn-success"> Clique aqui </a>>
+                 </div>
+            </div>
+        </div>
+
+        <div class="card-container">
+            <div class="card">
+                <div class="front">
+                <h2 class ="text-white my-4"> CURSINHO FFLCH</h2>
+                </div>
+                <div class="back">
+                    <h1 class="text-info"> CURSINHO FFLCH</h1>
+                    O cursinho da FFLCH foi criado por alunos e alunas da USP para dar oportunidades a jovens de baixa renda de estudarem para o vestibular.
+                    Atualmente contam com 4 turmas:
+                    Sábado - das 8h às 18h;
+                    Semana Extensivo e Semana Intensivo - das 14h às 19h15.
+                    <p><strong> Para mais informações sobre o cursinho FFLCH</strong></p>
+                    <a style="margin-top: 10px;"href="https://www.facebook.com/pg/cursinhodafflch/about/?ref=page_internal" class="btn btn-success"> Clique aqui </a>
+                 </div>
+            </div>
+        </div>
+
+        <div class="card-container">
+            <div class="card">
+                <div class="front">
+                <h2 class ="text-white my-4">CURSINHO DA CAPO</h2>
+                </div>
+                <div class="back">
+                    <h1 class="text-info"> CURSINHO DA CAPO</h1>
+                    O curso é gratuito e desenvolve conteúdos e habilidades referentes às áreas de Teoria Musical, Percepção e Rítmica, entre outras modalidades.
+                    Além de minimizar as deficiências musicais observadas nos alunos, orientando para o conteúdo exigido na prova e para metodologias de estudo, o projeto, que tem como professores alunos veteranos.
+                    <p><strong> Para mais informações sobre o cursinho da Capo</strong></p>
+                    <a style="margin-top: 10px;"href="https://cursinhodacapo.wordpress.com/processoseletivo/" class="btn btn-success"> Clique aqui </a>
+                 </div>
+            </div>
+        </div>
+
+        <div class="card-container">
+            <div class="card">
+                <div class="front">
+                <h2 class ="text-white my-4"> CURSINHO DA EPUFABC</h2>
+                </div>
+                <div class="back">
+                    <h1 class="text-info"> CURSINHO DA EPUFABC</h1>
+                    A Escola Preparatória da UFABC (EPUFABC) começou suas atividades em 2010.
+
+                    Desde o início, buscou-se promover o acesso das comunidades populares da região do ABC ao ensino superior de qualidade, oferecendo um cursinho gratuito preparatório para o Exame Nacional do Ensino Médio (ENEM)
+                    <p><strong> Para mais informações sobre o cursinho da EPUFABC</strong></p>
+                   <a style="margin-top: 10px;"href="https://proec.ufabc.edu.br/epufabc/" class="btn btn-success"> Clique aqui </a>
+                 </div>
+            </div>
+        </div>
+        
+        <div class="card-container">
+            <div class="card">
+                <div class="front">
+                <h2 class ="text-white my-4"> CURSINHO CPV</h2>
+                </div>
+                <div class="back">
+                    <h1 class="text-info"> CURSINHO CPV</h1>
+                    O CPV foi criado em 1959, iniciando suas atividades como Preparatório para o Vestibular da FGV, diferenciado dos demais por exigir um programa específico.
+                    Posteriormente, foi abrindo o seu leque de opções. Atualmente, mantém turmas exclusivas para a FGV (Administração, Direito e Economia), para o Insper e para a ESPM, além de turmas para as principais Universidades do país, comoUSP, Unesp, Unicamp, Unifesp, MACK, PUC, FAAP, Cásper Líbero e outras.
+                    <p><strong> Para mais informações sobre o cursinho CPV</strong></p>
+                    <a style="margin-top: 10px;"href="https://cpv.com.br/grupo-cpv/" class="btn btn-success"> Clique aqui </a>
+                 </div>
+            </div>
+        </div>
+
+        <div class="card-container">
+            <div class="card">
+                <div class="front">
+                <h2 class ="text-white my-4"> REDE EMANCIPA</h2>
+                </div>
+                <div class="back">
+                    <h1 class="text-info"> REDE EMANCIPA</h1>
+                    A Rede Emancipa é um movimento social de educação popular que desde 2007 constrói um importante trabalho voltado à educação de jovens de escolas públicas. O principal foco de atuação da Rede Emancipa tem sido a organização de cursinhos populares pré-universitários para atender à demanda represada dos estudantes de escolas públicas pelo acesso ao ensino superior em geral, e às universidades públicas em particular.
+                    <p><strong> Para mais informações sobre o cursinho Rede Emancipa</strong></p>
+
+                    <a style="margin-top: 10px;"href="https://redeemancipa.org.br/institucional/quem-somos/" class="btn btn-success"> Clique aqui </a>
+                 </div>
+            </div>
+        </div>
+
+        <div class="card-container">
+            <div class="card">
+                <div class="front">
+                <h2 class ="text-white my-4"> CURSINHO FATEC</h2>
+                </div>
+                <div class="back">
+                    <h1 class="text-info"> CURSINHO FATEC</h1>
+                    O Cursinho Preparatório FATEC/ETEC, curso Pré-vestibulinho e Pré-vestibular e Enem, é um dos mais importantes projetos desenvolvidos por alunos e Ex. Alunos da Fatec, um exemplo de que a universidade e as escolas técnicas públicas não são, apenas, um espaço para pensar e refletir o mundo, mas também para transformá-lo
+                    <p><strong> Para mais informações sobre o cursinho FATEC</strong></p>
+                    <a style="margin-top: 10px;"href="https://cursinhofatec.com.br/" class="btn btn-success"> Clique aqui </a>
+                 </div>
+            </div>
+        </div>
+
+        <div class="card-container">
+            <div class="card">
+                <div class="front">
+                <h2 class ="text-white my-4"> CURSINHO MEDENSINA</h2>
+                </div>
+                <div class="back">
+                    <h1 class="text-info"> CURSINHO MEDENSINA</h1>
+                    O Curso Pré-Vestibular MedEnsina foi criado em 2002 por alunos da Faculdade de Medicina da Universidade de São Paulo (FM-USP) com apoio do Professor Doutor Paulo Saldiva, do Centro Acadêmico Oswaldo Cruz e da instituição.
+                    O curso é gratuito e voltado para pessoas que não tenham condições de se manter em um cursinho particular. O material também é gratuito, doado por um grande Cursinho Pré-Vestibular.
+                    <p><strong> Para mais informações sobre o cursinho MedEnsina</strong></p>
+                    <a style="margin-top: 10px; "href="http://medensina.com/" class="btn btn-success"> Clique aqui </a>
+                 </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class ="col-12 text-center my-5">
+    </div>
+
+    </main>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<?php require_once('./nav/footer.html') ?>
+
+
+</body>
+
+</html>
