5ba25222e01e2e61f8e757680354cde1fa7786e0
Gustavo Castanho <65131471+duartecgustavo@users.noreply.github.com>
2021-01-19 16:05:13 -0300

    ATT

---
+++ teoria.php
@@ -0,0 +1,530 @@
+<?php
+//require_once('./servidor/Conection.php');
+require_once('./nav/menu.html');
+?>
+      <main class="row">
+        <div class="d-flex align-items-center justify-content-center py-4 col-lg-12">
+          <div class="text-center">
+            <h1 class="display-6" id="topo">
+              <strong>
+                🧐 Teoria das Múltiplas Inteligências: o que é isso?
+              </strong>
+            </h1>
+          </div>
+        </div>
+
+        <div class="d-flex align-items-center justify-content-center col-lg-12">
+          <div
+            class="embed-responsive embed-responsive-16by9 my-4 mx-auto shadow d-flex justify-content-center align-content-center"
+            style={{ width: "80%" }}
+          >
+            <iframe
+              width="1280"
+              height="720"
+              src="https://www.youtube.com/embed/oJ85PMYqHqg"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+
+        <p></p>
+        <p></p>
+
+        <h1 class="text-center teoriaTitulo1">
+          <strong> Quais são os oito tipos de inteligência? </strong>
+        </h1>
+
+        <p class="text-center teoriaTitulo2">
+          {" "}
+          Conheça os oito perfis definidos pela Teoria das Múltiplas
+          Inteligências e aprenda a identificar qual é a sua!{" "}
+        </p>
+ 
+        <p></p>
+        <hr></hr>
+
+        <p class="text-center teoriaTitulo2">
+          <strong> LÓGICO MATEMÁTICA </strong>
+        </p>
+
+        <div class="displayed">
+          <img class="displayed"
+            src="https://super.abril.com.br/wp-content/uploads/2018/07/8-inteligencias-2.png"
+            border="0"
+            alt=""
+            width="615"
+            height="360"
+          >
+          </img>
+        </div>
+
+        <div class="container"> 
+        <p class="teoriaCorpo1">
+          {" "}
+          É a habilidade para desenvolver equações, resolver cálculos e resolver
+          problemas abstratos. Sua manifestação é uma das que as pessoas
+          consideram como inteligência tradicional, especialmente na escola. Ela
+          é típica de quem manda bem em contas, em lógica e em estratégia.{" "}
+        </p>
+        </div>
+
+        <div class="container"> 
+        <ul class="teoriaCorpo1">
+          <li>
+            <strong>Hobbies indicados:</strong> quebra-cabeça, xadrez,
+            videogame, computadores, desvendar mistérios.
+          </li>
+          <li>
+            <strong>Carreiras indicadas:</strong> ciências, computação, direito,
+            engenharia, matemática, medicina.
+          </li>
+          <li>
+            <strong>Gênios desse tipo: </strong>
+            
+            <a href="https://pt.wikipedia.org/wiki/Albert_Einstein" target="_blank">
+            Albert Einstein, </a>
+            <a href="https://pt.wikipedia.org/wiki/Isaac_Newton" target="_blank">
+            Isaac Newton, </a>
+            <a href="https://pt.wikipedia.org/wiki/Stephen_Hawking" target="_blank">
+            Stephen Hawkin, </a>
+            <a href="https://pt.wikipedia.org/wiki/Mark_Zuckerberg" target="_blank">
+            Mark Zuckerberg. </a>
+          </li>
+        </ul>
+        </div>
+        
+
+        <p></p>
+        <p></p>
+        <hr></hr>
+
+        <p class="text-center teoriaTitulo2">
+          <strong> LINGUÍSTICA </strong>
+        </p>
+
+        <div class="displayed">
+          <img class="displayed"
+            src="https://super.abril.com.br/wp-content/uploads/2018/07/8-inteligencias-3.png"
+            border="0"
+            alt=""
+            width="615"
+            height="360"
+          >
+          </img>
+        </div>
+
+        <div class="container"> 
+        <p class="teoriaCorpo1">
+          {" "}
+          Algumas pessoas têm muita facilidade para lidar com palavras. Elas
+          possuem um tipo de inteligência que permite analisar informações e
+          desenvolver produtos de linguagem escrita e oral, como discursos e
+          livros, sempre encontrando o melhor jeito de comunicar grandes ideias.<br/>
+          É bastante requisitada (e apreciada) na escola, já que a maioria das
+          provas e exercícios são por escrito.{" "}
+        </p>
+        </div>
+
+        <div class="container"> 
+        <ul class="teoriaCorpo1">
+          <li>
+            <strong>Hobbies indicados:</strong> contar histórias, escrever
+            textos, declamar poesias, fazer passatempos com palavras, ler, jogar
+            RPG.
+          </li>
+          <li>
+            <strong>Carreiras indicadas:</strong> editoração, educação,
+            jornalismo, literatura, tradução.
+          </li>
+          <li>
+            <strong>Gênios desse tipo: </strong>Cecília Meirelles, Gabriel
+            García Marquez, William Shakespeare, Tom Wolfe.
+          </li>
+        </ul>
+        </div>
+
+        <p></p>
+        <p></p>
+        <hr></hr>
+
+        <p class="text-center teoriaTitulo2">
+          <strong> MUSICAL </strong>
+        </p>
+
+        <div class="displayed">
+          <img class="displayed"
+            src="https://super.abril.com.br/wp-content/uploads/2018/07/8-inteligencias-4.png"
+            border="0"
+            alt=""
+            width="615"
+            height="360"
+          >
+          </img>
+        </div>
+
+        <div class="container"> 
+        <p class="teoriaCorpo1">
+          {" "}
+          O nome já entrega: é aquela que permite a uma pessoa produzir,
+          recordar e estabelecer sentido em diferentes padrões de som. <br/>
+          A sensibilidade musical se manifesta ao ouvir, cantar, compor e tocar
+          instrumentos.
+        </p>
+        </div>
+
+        <div class="container"> 
+        <ul class="teoriaCorpo1">
+          <li>
+            {" "}
+            <strong>Hobbies indicados:</strong> assobiar, cantar, fazer rimas,
+            compor, participar de um coral, ouvir música, tocar instrumentos.
+          </li>
+          <li>
+            <strong>Carreiras indicadas:</strong> canto, composição, crítica
+            musical, direção musical, DJ, ensino de música, engenharia de som
+            para filmes e peças.
+          </li>
+          <li>
+            <strong>Gênios desse tipo: </strong>Ludwig van Beethoven, Tom Jobim,
+            Wolfgang Amadeus Mozart.
+          </li>
+        </ul>
+        </div>
+
+        <p></p>
+        <p></p>
+        <hr></hr>
+
+        <p class="text-center teoriaTitulo2">
+          <strong> NATURALÍSTICA </strong>
+        </p>
+
+        <div class="displayed">
+          <img class="displayed"
+            src="https://super.abril.com.br/wp-content/uploads/2018/07/8-inteligencias-5.png"
+            border="0"
+            alt=""
+            width="615"
+            height="360"
+          >
+          </img>
+        </div>
+
+      <div class="container"> 
+        <p class="teoriaCorpo1">
+          {" "}
+          Típica daqueles que têm uma forte ligação com a natureza e uma
+          habilidade incomum para identificar e distinguir animais, plantas,
+          formações climáticas e outros elementos do mundo natural. Na escola,
+          quem tem essa inteligência bombando se dá bem em biologia e geografia.
+        </p>
+        </div>
+
+      <div class="container"> 
+        <ul class="teoriaCorpo1">
+          <li>
+            <strong>Hobbies indicados:</strong> cuidar de plantas e jardins,
+            fazer trilhas e caminhadas na natureza, passear com o cachorro.
+          </li>
+          <li>
+            <strong> Carreiras indicadas:</strong> biologia, ecologia, geologia,
+            medicina, medicina veterinária, meteorologia.
+          </li>
+          <li>
+            <strong>Gênios desse tipo: </strong>Charles Darwin, Alexander von
+            Humboldt, Aziz Ab&#8217;Saber, Chico Mendes.
+          </li>
+        </ul>
+        </div>
+
+        <p></p>
+        <p></p>
+        <hr></hr>
+
+        <p class="text-center teoriaTitulo2">
+          <strong> CORPORAL-CINESTÉSICA </strong>
+        </p>
+
+        <div class="displayed">
+          <img class="displayed"
+            src="https://super.abril.com.br/wp-content/uploads/2018/07/8-inteligencias-6.png"
+            border="0"
+            alt=""
+            width="615"
+            height="360"
+          >
+          </img>
+        </div>
+
+        <div class="container"> 
+        <p class="teoriaCorpo1">
+          {" "}
+          É a capacidade de usar o próprio corpo (e seus movimentos) para
+          solucionar problemas ou criar produtos. É a mais requisitada, por
+          exemplo, nos craques de futebol, nos ginastas, nos bailarinos e nos
+          artistas em geral.
+        </p>
+        </div>
+
+        <div class="container"> 
+        <ul class="teoriaCorpo1">
+          <li>
+            <strong>Hobbies indicados:</strong> esportes, dança, mágica, teatro,
+            mímica, malabarismo, massagem.
+          </li>
+          <li>
+            <strong>Carreiras indicadas:</strong> coreografia, dança, artes
+            dramáticas, artes plásticas, educação física, esportes.
+          </li>
+          <li>
+            <strong>Gênios desse tipo: </strong>
+            <span>Lionel Messi, Marta, Meryl Streep, Michael Phelps.</span>
+          </li>
+        </ul>
+        </div>
+
+        <p></p>
+        <p></p>
+        <hr></hr>
+
+        <p class="text-center teoriaTitulo2">
+          <strong> ESPACIAL </strong>
+        </p>
+
+        <div class="displayed">
+          <img class="displayed"
+            src="https://super.abril.com.br/wp-content/uploads/2018/07/8-inteligencias-7.png"
+            border="0"
+            alt=""
+            width="615"
+            height="360"
+          >
+          </img>
+        </div>
+
+        <div class="container"> 
+        <p class="teoriaCorpo1">
+          {" "}
+          Tem gente que não sabe se orientar num mapa, não consegue estimar
+          medidas e não é capaz de montar um bom look. Por outro lado, os
+          mestres dessa inteligência têm uma habilidade extra para reconhecer o
+          espaço e pensar em termos de cores, formas e medidas, manipulando
+          tanto as grandes quanto as pequenas imagens.
+        </p>
+        </div>
+
+        <div class="container"> 
+        <ul class="teoriaCorpo1">
+          <li>
+            <strong>Hobbies indicados:</strong> aeromodelismo, artesanato,
+            desenho, fotografia, pintura, escultura.
+          </li>
+          <li>
+            <strong>Carreiras indicadas:</strong> arquitetura, artes plásticas,
+            aviação, desenho industrial, design de interiores, design gráfico,
+            engenharia.
+          </li>
+          <li>
+            <strong>Gênios desse tipo: </strong>Coco Chanel, Oscar Niemeyer,
+            Pablo Picasso, Santos Dumont.
+          </li>
+        </ul>
+        </div>
+
+        <p></p>
+        <p></p>
+        <hr></hr>
+
+        <p class="text-center teoriaTitulo2">
+          <strong> INTERPESSOAL </strong>
+        </p>
+
+        <div class="displayed">
+          <img class="displayed"
+            src="https://super.abril.com.br/wp-content/uploads/2018/07/8-inteligencias-8.png"
+            border="0"
+            alt=""
+            width="615"
+            height="360"
+          >
+          </img>
+        </div>
+
+        <div class="container"> 
+        <p class="teoriaCorpo1">
+          {" "}
+          Sabe aquele amigo que é o melhor ombro na hora que você precisa
+          chorar? Que entende fácil tudo que você está sentindo, sabe te
+          consolar e sabe propor soluções para os problemas? Ele é bem
+          desenvolvido nesse tipo de inteligência: a que reconhece e trabalha os
+          desejos e humores dos outros. Ela é útil na hora de estudar em grupo,
+          por exemplo, ou de gerenciar uma equipe.
+        </p>
+        </div>
+
+        <div class="container"> 
+        <ul class="teoriaCorpo1">
+          <li>
+            <strong>Hobbies indicados:</strong> trabalho voluntário, esportes em
+            grupo, conversas.
+          </li>
+          <li>
+            <strong>Carreiras indicadas:</strong> jornalismo, marketing,
+            política, psicologia, recursos humanos, relações públicas, teologia,
+            vendas.
+          </li>
+          <li>
+            <strong>Gênios desse tipo: </strong>Winston Churchill, Mahatma
+            Gandhi, Marília Gabriela, Papa Francisco.
+          </li>
+        </ul>
+        </div>
+
+        <p></p>
+        <p></p>
+        <hr></hr>
+
+        <p class="text-center teoriaTitulo2">
+          <strong> INTRAPESSOAL </strong>
+        </p>
+
+        <div class="displayed">
+          <img class="displayed"
+            src="https://super.abril.com.br/wp-content/uploads/2018/07/8-inteligencias-9.png"
+            border="0"
+            alt=""
+            width="615"
+            height="360"
+          >
+          </img>
+        </div>
+
+        <div class="container"> 
+        <p class="teoriaCorpo1">
+          {" "}
+          É o inverso da interpessoal: o indivíduo usa a inteligência para se
+          autoconhecer. São as pessoas com grande grau de autonomia, capazes de
+          reconhecer suas próprias intenções, motivações e opiniões. Essa
+          autoanálise pode colocá-las em boa posição para refletir sobre a
+          condição humana, seja por meio de textos filosóficos, seja por meio de
+          grandes romances.
+        </p>
+        </div>
+
+        <div class="container"> 
+        <ul class="teoriaCorpo1">
+          <li>
+            <strong>Hobbies indicados:</strong> escrever um diário, meditar,
+            planejar seus objetivos de vida.
+          </li>
+          <li>
+            <strong>Carreiras indicadas:</strong> filosofia, literatura,
+            psicologia, teologia.
+          </li>
+          <li>
+            <strong>Gênios desse tipo: </strong>Ernest Hemingway, Machado de
+            Assis, Friedrich Nietzsche, Simone de Beauvoir.
+          </li>
+        </ul>
+        </div>
+
+        <p></p>
+        <p></p>
+        <hr></hr>
+
+        <div class="container"> 
+        <h3></h3>
+        <h3>Como identifico a minha?</h3>
+        <p>
+          <em>
+            A autoanálise pode dar algumas pistas do seu potencial
+          </em>
+        </p>
+        <div class="box">
+          <p>
+            <strong>
+              <br />
+              SEM FÓRMULAS
+            </strong>
+          </p>
+          <p>
+            Não há um teste padronizado para detectar a inteligência
+            predominante de um indivíduo. Isso iria contra a própria Teoria das
+            Inteligências Múltiplas: se ela é múltipla, não dá para medi-la numa
+            única escala, como o exame de QI. Seria preciso desenvolver
+            avaliações de desempenho e{" "}
+            <strong>parâmetros para cada uma.</strong> Além disso, Gardner
+            ressalta que &#8220;as inteligências devem ser mobilizadas para
+            ajudar os indivíduos a aprender, e não como uma forma de
+            categorizá-los&#8221;.
+          </p>
+          <br/>
+          <p>
+            <strong>TOQUE DE MIDAS</strong>
+          </p>
+          <p>
+            Gardner não acredita em testes, mas alguns seguidores da Teoria da
+            Inteligências Múltiplas já tentaram sistematizá-las (e
+            transformá-las numa forma de ganhar dinheiro). O mais famoso é o
+            Midas, criado pelo consultor Branton Shearer, que pode ser utilizado
+            como um <strong>teste vocacional.</strong> O próprio Gardner
+            reconhece que ele mede, sim, habilidades em certas áreas &#8211; mas
+            não todas. E ainda depende da pessoa se conhecer bem (ou seja, ter
+            alta inteligência interpessoal!)
+          </p>
+          <br/>
+          <p>
+            <strong>UMA PAUSA PARA PENSAR</strong>
+          </p>
+          <p>
+            Testes podem não medir com precisão as capacidades de cada um, mas
+            um pouco de autorreflexão não faz mal a ninguém.{" "}
+            <strong>Pense nas perguntas abaixo</strong> e compare com as
+            descrições dos oito tipos de inteligência para ver qual delas tem
+            mais a ver com você:
+          </p>
+          <ul>
+            <li>
+              Que tipos de atividades mais atraem sua atenção? E qual é{" "}
+              <span class="textoDestaque">seu</span> nível de aptidão para elas?
+            </li>
+            <li>Em quais matérias da escola você se sai melhor?</li>
+            <li>
+              Que métodos e ferramentas você usa para se expressar ou resolver
+              problemas?
+            </li>
+            <li>
+              Quando memoriza uma cena, o que nela chama mais sua atenção?
+            </li>
+            <li>Que carreira você pensa em seguir?</li>
+          </ul>
+        </div>
+        <br/>
+        <h6 class="teoriaFonte">
+          <em>
+            Fonte: <br/>
+            Portal da Revista SuperInteressante, fevereiro de 2020 <br/>
+            Textos por Julia Moióli <br/>
+            Ilustrações por Bruno Miranda <br/>
+          </em>
+        </h6>
+          
+        <div class="d-flex align-items-center justify-content-center col-12 p-4">
+          <a class="btn btn-white" href="#topo">
+            Voltar ao topo
+          </a>
+        </div>
+        </div>
+      </main>
+      </script>    
+</body>
+
+<footer class="container-fluid bg-purple text-light d-flex justify-content-center">
+    <div class="footerBar d-flex align-items-center">
+        ©2021 - EduImpulso - ORIENTAÇÃO PROFISSIONAL
+    </div>
+</footer>
+
+</html>
