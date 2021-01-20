9b2f724758f34431c60b09228af586fa45b9e5b3
Gustavo Castanho <65131471+duartecgustavo@users.noreply.github.com>
2021-01-20 18:10:56 -0300

    Update lista_curso.php

--- lista_curso.php
+++ lista_curso.php
@@ -6,23 +6,71 @@ if(!isset($_SESSION['id_user'])){
 }*/
 require_once('./nav/menu.html');
 ?>
-<main class="container d-flex justify-content-center align-items-center">
+<div class="container d-flex justify-content-center align-items-center my-3">
     <div class="row d-flex justify-content-center">
-        <?php
-            $json = file_get_contents("http://localhost/eduimpulso/servidor/apicourses.php");
-            $data = json_decode($json, true);
-            foreach ($data as $key => $row){
-        ?>
-            <div class="list-group col-12 m-1">
-                <a href="./cursos_tecnicos.php?id<?php echo $row["id_curso"];?>" class="list-group-item list-group-item-action">
-                    <?php echo $row["nome"];?>
+
+    <div class="list-group col-12 m-1">
+                <a href="./cursos_tecnicos.php?id1" class="list-group-item list-group-item-action">
+                    Administração
                 </a>
             </div>
-        <?php
-            }
-        ?>
+
+    <div class="list-group col-12 m-1">
+                <a href="./cursos_tecnicos.php?id1" class="list-group-item list-group-item-action">
+                    Administração
+                </a>
+            </div>
+
+    <div class="list-group col-12 m-1">
+                <a href="./cursos_tecnicos.php?id1" class="list-group-item list-group-item-action">
+                    Administração
+                </a>
+            </div>
+
+    <div class="list-group col-12 m-1">
+                <a href="./cursos_tecnicos.php?id1" class="list-group-item list-group-item-action">
+                    Administração
+                </a>
+            </div>
+
+    <div class="list-group col-12 m-1">
+                <a href="./cursos_tecnicos.php?id1" class="list-group-item list-group-item-action">
+                    Administração
+                </a>
+            </div>
+
+    <div class="list-group col-12 m-1">
+                <a href="./cursos_tecnicos.php?id1" class="list-group-item list-group-item-action">
+                    Administração
+                </a>
+            </div>
+
+    <div class="list-group col-12 m-1">
+                <a href="./cursos_tecnicos.php?id1" class="list-group-item list-group-item-action">
+                    Administração
+                </a>
+            </div>
+
+    <div class="list-group col-12 m-1">
+                <a href="./cursos_tecnicos.php?id1" class="list-group-item list-group-item-action">
+                    Administração
+                </a>
+            </div>
+
+    <div class="list-group col-12 m-1">
+                <a href="./cursos_tecnicos.php?id1" class="list-group-item list-group-item-action">
+                    Administração
+                </a>
+            </div>
+
+    <div class="list-group col-12 m-1">
+                <a href="./cursos_tecnicos.php?id1" class="list-group-item list-group-item-action">
+                    Administração
+                </a>
+            </div>
+
     </div>
-</main>
+</div>
 
 <?php
 require_once('./nav/footer.html');
