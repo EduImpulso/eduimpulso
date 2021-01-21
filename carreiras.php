9fd0697e4592291aaba96237037e6b1a35ffad46
Gustavo Castanho <65131471+duartecgustavo@users.noreply.github.com>
2021-01-20 21:56:38 -0300

    ajustes

--- carreiras.php
+++ carreiras.php
@@ -6,24 +6,29 @@ if(!isset($_SESSION['id_user'])){
 }
 require_once('./nav/menu.html');
 ?>
+<div class="cotainer-fluid">
+    <div class="d-flex align-items-center justify-content-center my-3">
+        <h3>Carreiras 👨‍🚀</h3>
+    </div>
+<div class="linha mx-auto"></div>
 <div class="container d-flex justify-content-center align-items-center my-3">
     <div class="row d-flex justify-content-center">
-        <?php
+    <?php
             $json = file_get_contents("http://localhost/eduimpulso/servidor/apicourses.php");
             $data = json_decode($json, true);
             foreach ($data as $key => $row){
                 $id = $row['id_curso']
         ?>
-            <div class="list-group col-12 m-1">
-                <a href="<?php echo"./servidor/course.php?id=$id"?>" class="list-group-item list-group-item-action">
+        <div class="list-group col-12 m-1">
+            <a href="<?php echo"./servidor/course.php?id=$id";?>" class="btnCarreira list-group-item">
                     <?php echo $row["nome"];?>
                 </a>
             </div>
         <?php
             }
         ?>
-
     </div>
+</div>
 </div>
 
 <?php
