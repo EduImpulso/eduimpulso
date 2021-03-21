3d8c11eacab554268ae9f706186233e9f62106d4
flaviogomesbr <flaviogonasc@gmail.com>
2021-03-21 13:28:17 -0300

    updt carreiras

--- src/components/Carreiras/conteudo.jsx
+++ src/components/Carreiras/conteudo.jsx
@@ -49,7 +49,7 @@ const Conteudo = () =>{
       <CardCarreirasAlf h5='Edificações' p="Tipo de Curso: Técnico Duração: 4 semestres Modalidade: Presencial" /> <br/>
       <CardCarreirasAlf h5='Edificações - Integrado ao Ensino Médio' p="Tipo de Curso: Técnico Duração: 6 semestres Modalidade: Presencial" /> <br/>
       <CardCarreirasAlf h5='Eletrônica - Integrado ao Ensino Médio' p="Tipo de Curso: Técnico Duração: 4 semestres Modalidade: Presencial" /> <br/>
-      <CardCarreirasAlf h5='Ensio Médio - com Habilitação Profissional em Administração ' p="Tipo de Curso: Técnico Duração: 6 semestres Modalidade: Presencial" /> <br/>
+      <CardCarreirasAlf h5='Ensino Médio - com Habilitação Profissional em Administração ' p="Tipo de Curso: Técnico Duração: 6 semestres Modalidade: Presencial" /> <br/>
       <CardCarreirasAlf h5='Ensino Médio - com Habilitação Profissional de Técnico em Informática' p="Tipo de Curso: Técnico Duração: 6 semestres Modalidade: Presencial" /> <br/>
       <CardCarreirasAlf h5='Eletroeletrônica ' p="Tipo de Curso: Técnico Duração: 4 semestres Modalidade: Presencial" /> <br/>
 
